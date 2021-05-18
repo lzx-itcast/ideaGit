e44defa12f94f0cbeab60cc4541271f6c1bef913
lili <lili@itcast.cn>
2021-05-18 23:34:59 +0800

    version2

--- GitTest/src/bigdata/itcast/cn/HelloPrint.java
+++ GitTest/src/bigdata/itcast/cn/HelloPrint.java
@@ -3,5 +3,6 @@ package bigdata.itcast.cn;
 public class HelloPrint {
     public static void main(String[] args) {
         System.out.println("Hello,itcast");
+        System.out.println("Hello,itcast,ieheima");
     }
 }
